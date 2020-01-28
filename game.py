bae8cac228971403e7ae72b0558a46cea0e5bc86
Your Name <you@example.com>
2020-01-28 09:59:44 -0800

    Finished defining variables

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 """A number-guessing game."""
+from random import randint
 
 # Put your code here
 print("Howdy, what's your name?")
 name = input("(type in your name) ")
 print(f"{name}, I'm thinking of a number between 1 and 100.")
+print("Try to guess my number.")
+random_num = randint(1, 100)
