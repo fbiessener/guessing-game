d3cc9648fa85570b3b891d8f4e5815658d3d7089
Your Name <you@example.com>
2020-01-28 10:02:28 -0800

    New strings, break for lunch

--- game.py
+++ game.py
@@ -7,3 +7,8 @@ name = input("(type in your name) ")
 print(f"{name}, I'm thinking of a number between 1 and 100.")
 print("Try to guess my number.")
 random_num = randint(1, 100)
+
+# "Your guess? "
+# "Your guess is too high, try again."
+# "Your guess is too low, try again."
+# "Well done, " + name + " You found my number in " + 
